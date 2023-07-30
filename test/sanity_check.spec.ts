609fb5d89d8b1840f1a1b8218b420e9708df653e
Rychu <pawel.rychlicki@gmail.com>
2023-07-30 15:19:51 +0200

    Added unit tests

--- test/sanity_check.spec.ts
+++
@@ -1,6 +0,0 @@
-import test from "ava";
-
-test(`Sanity check`, t => {
-    // Assert
-    t.true(true);
-});
--- test/spawn.spec.ts
+++ test/spawn.spec.ts
@@ -1,67 +1,198 @@
 import { ChildProcess } from "child_process";
+import path from "path";
 
 import test from "ava";
 
-import spawn from "../src/spawn.js";
+import spawn from "../src/index.js";
 
-test(`Successfully spawns`, async t => {
+import type { SpawnError, SpawnReturnValue } from "../src/index.js";
+
+test.serial(`Successfully spawns`, async t => {
     // Arrange
-    const sutMethod = () => spawn(`node`, [`-v`]);
+    let exitCode = -1;
 
-    // Act & Assert
+    const sutMethod = async () => {
+        exitCode = await spawn(`node`, [`-v`]);
+    };
+
+    // Act
     await t.notThrowsAsync(sutMethod);
+
+    // Assert
+    t.is(exitCode, 0);
 });
 
-test(`Returns child inside promise`, t => {
-    // Arrange & Act
+test.serial(`Returns child inside promise`, async t => {
+    // Arrange
     const promise = spawn(`node`, [`-v`]);
 
+    // Act
+    const child = promise.child;
+    const exitCode = await promise;
+
     // Assert
-    t.truthy(promise.child);
-    t.true(promise.child instanceof ChildProcess);
+    t.truthy(child);
+    t.true(child instanceof ChildProcess);
+    t.is(exitCode, child.exitCode || 0);
 });
 
-test(`Resolves when exit code is null`, t => {
+test.serial(`Resolves when exit code and signal are null`, async t => {
+    // Arrange
+    let promise: SpawnReturnValue | undefined;
+    let exitCode = -1;
+
+    const sutMethod = async () => {
+        promise = spawn(`node`, [`-v`]);
+        promise.child.emit(`close`, null, null);
+        exitCode = await promise;
+    };
+
+    // Act
+    await t.notThrowsAsync(sutMethod);
+
     // Assert
-    t.true(true);
+    t.is(exitCode, 0);
 });
 
-test(`Spawns with right args`, t => {
+test.serial(`Spawns with correct parameters`, async t => {
+    // Arrange
+    const spawnArgs = [`-v`];
+    const spawnFile = `node`;
+
+    // Act
+    const promise = spawn(`node`, spawnArgs);
+
     // Assert
-    t.true(true);
+    t.is(promise.child.spawnfile, spawnFile);
+    t.deepEqual(promise.child.spawnargs, [spawnFile, ...spawnArgs]);
 });
 
-test(`Spawns with right options`, t => {
+test.serial(`Rejects on error`, async t => {
+    // Arrange
+    const sutMethod = async () => await spawn(`dummyCommandThatDoesn'tExist`);
+
+    // Act & Assert
+    const error = await t.throwsAsync(sutMethod);
+
     // Assert
-    t.true(true);
+    t.truthy(error?.message);
+    t.true(error!.message!.indexOf(`ENOENT`) >= 0);
 });
 
-test(`Rejects on error`, t => {
+test.serial(`Rejects with correct signal`, async t => {
+    // Arrange
+    const signal = `SIGKILL`;
+
+    const scriptPath = path.join(process.cwd(), `test/commands/fiveSecondsScript.sh`);
+
+    const sutMethod = async () => {
+        const promise = spawn(`bash`, [scriptPath]);
+        promise.child.kill(signal);
+        await promise;
+    };
+
+    // Act & Assert
+    const error = await t.throwsAsync(sutMethod);
+
     // Assert
-    t.true(true);
+    t.is((error as SpawnError).signal, signal);
 });
 
-test(`Writes to stdout when no stdout listener passed`, t => {
+test.serial(`Rejects with correct exit code`, async t => {
+    // Arrange
+    let promise: SpawnReturnValue | undefined;
+
+    const scriptPath = path.join(process.cwd(), `test/commands/exitNonZero.sh`);
+
+    const sutMethod = async () => {
+        promise = spawn(`bash`, [scriptPath]);
+        await promise;
+    };
+
+    // Act
+    await t.throwsAsync(sutMethod);
+
     // Assert
-    t.true(true);
+    t.is(promise?.child.exitCode, 12);
 });
 
-test(`Writes to stderr when no stderr listener passed`, t => {
+test.serial(`Writes to console.log when no stdout listener passed`, async t => {
+    // Arrange
+    let message: string | undefined;
+
+    const originalConsoleImplementation = console.log;
+
+    console.log = (data: string) => (message = data);
+
+    // Act
+    await spawn(`node`, [`-v`]);
+
+    console.log = originalConsoleImplementation;
+
     // Assert
-    t.true(true);
+    t.is(message?.trim(), process.version);
 });
 
-test(`Does not write to stdout when stdout listener passed`, t => {
+test.serial(`Writes to console.error when no stderr listener passed`, async t => {
+    // Arrange
+    let message: string | undefined;
+
+    const originalConsoleImplementation = console.error;
+
+    console.error = (data: string) => (message = data);
+
+    const scriptPath = path.join(process.cwd(), `test/commands/stderror.sh`);
+
+    // Act
+    await spawn(`bash`, [scriptPath]);
+
+    console.error = originalConsoleImplementation;
+
     // Assert
-    t.true(true);
+    t.is(message?.trim(), `error message text`);
 });
 
-test(`Does not write to stderr when stderr listener passed`, t => {
+test.serial(`Does not write to console when stdout listener passed`, async t => {
+    // Arrange
+    let message: string | undefined;
+
+    const listener = () => { };
+
+    const originalConsoleImplementation = console.log;
+
+    console.log = (data: string) => (message = data);
+
+    // Act
+    await spawn(`node`, [`-v`], { stdoutListener: listener });
+
+    console.log = originalConsoleImplementation;
+
     // Assert
-    t.true(true);
+    t.is(message, undefined);
 });
 
-test(`Calls stdout listener when passed`, async t => {
+test.serial(`Does not write to console when stderr listener passed`, async t => {
+    // Arrange
+    let message: string | undefined;
+
+    const listener = () => { };
+
+    const originalConsoleImplementation = console.error;
+
+    console.error = (data: string) => (message = data);
+
+    const scriptPath = path.join(process.cwd(), `test/commands/stderror.sh`);
+
+    // Act
+    await spawn(`bash`, [scriptPath], { stderrListener: listener });
+
+    console.error = originalConsoleImplementation;
+
+    // Assert
+    t.is(message, undefined);
+});
+
+test.serial(`Calls stdout listener when passed`, async t => {
     // Arrange
     let message: string | undefined;
 
@@ -74,12 +205,17 @@ test(`Calls stdout listener when passed`, async t => {
     t.is(message?.trim(), process.version);
 });
 
-test(`Calls stderr listener when passed`, t => {
-    // Assert
-    t.true(true);
-});
+test.serial(`Calls stderr listener when passed`, async t => {
+    // Arrange
+    let message: string | undefined;
 
-test(`Rejects on not 0 exit code`, t => {
+    const scriptPath = path.join(process.cwd(), `test/commands/stderror.sh`);
+
+    const listener = (data: string | Buffer) => (message = data.toString());
+
+    // Act
+    await spawn(`bash`, [scriptPath], { stderrListener: listener });
+
     // Assert
-    t.true(true);
+    t.is(message?.trim(), `error message text`);
 });
